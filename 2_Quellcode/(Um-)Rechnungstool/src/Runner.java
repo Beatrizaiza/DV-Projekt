4ef46e35cd1263521218db0167d471b979818d13
lucawld <102234131+lucawld@users.noreply.github.com>
2022-04-25 01:25:37 +0200

    eigener Quellcode und GUI
    Application Template kopiert und umgeschrieben um Hauptfenster GUI zu erzeugen
    Bilder hinzugefügt

---
+++ 2_Quellcode/(Um-)Rechnungstool/src/Runner.java
@@ -0,0 +1,90 @@
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Runner
+{
+	
+	public static void main(String[] args)
+	{
+		//Erzeugt Hauptfenster//
+		JFrame hauptfenster = new JFrame("Umrechnungstool");
+        hauptfenster.setBounds(350, 100, 850, 600); //legt Größe und Position fest//
+        hauptfenster.setVisible(true); //macht Fenster sichtbar//
+        hauptfenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Befehl beim Schließen des Fensters//
+        
+        hauptfenster.addWindowListener(new WindowAdapter() { 
+        	@Override
+            public void windowClosing(WindowEvent e)
+        	{
+        		System.out.println("Das Fenster wurde geschlossen. Verlassen ...");
+        		System.exit(0); 
+        	}
+        });
+               
+        //--- create a label, a text field, a fly button and a custom drawing canvas ---//
+        JLabel label = new JLabel("Wählen Sie zwischen folgenden Rechnern:");
+        JButton einheitenumrechner = new JButton("Einheitenumrechner");
+        JButton flaechenrechner = new JButton("Flächenrechner");
+        JButton volumenrechner = new JButton("Volumenrechner");
+        JButton temperaturumrechner = new JButton("Temperaturumrechner");
+
+        //Befehl zum Öffnen des Einheitenumrechners//
+        einheitenumrechner.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.out.println("Weiterleitung zum Einheitenumrechner");
+				
+			}
+        });
+			
+		//Befehl zum Öffnen des Flächenrechners//
+	    flaechenrechner.addActionListener(new ActionListener() {
+	    	@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				System.out.println("Weiterleitung zum Einheitenumrechner");
+					
+			}	
+		});
+	    
+		  //Befehl zum Öffnen des Volumenrechners//
+	        volumenrechner.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) 
+				{
+					System.out.println("Weiterleitung zum Volumenrechner");
+					
+				}
+	        });
+	        
+	      //Befehl zum Öffnen des Temperaturumrechners//
+	        volumenrechner.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) 
+				{
+					System.out.println("Weiterleitung zum Temperaturumrechner");
+					
+				}
+	        });	 
+        
+        //--- add components to the main window ---//
+        Container pane = hauptfenster.getContentPane();
+        pane.setLayout(new FlowLayout());
+        pane.add(label);
+        pane.add(einheitenumrechner);
+        pane.add(flaechenrechner);
+        pane.add(volumenrechner);
+        pane.add(temperaturumrechner);
+           
+	}
+}
