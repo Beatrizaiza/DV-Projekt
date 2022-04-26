a2a6b2459c849639224af03787124d06b03ce560
lucawld <102234131+lucawld@users.noreply.github.com>
2022-04-26 16:17:56 +0200

    Kleinigkeiten rausgelöscht

--- 2_Quellcode/(Um-)Rechnungstool/src/Runner.java
+++ 2_Quellcode/(Um-)Rechnungstool/src/Runner.java
@@ -1,16 +1,12 @@
 
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
