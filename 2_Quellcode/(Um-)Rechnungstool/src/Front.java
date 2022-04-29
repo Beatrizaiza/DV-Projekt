6b81e6e7b96c9a31ba0d955628c9ff8407c1d0e5
leonwelker <102161952+leonwelker@users.noreply.github.com>
2022-04-29 14:11:20 +0200

    Metrisch-imperial 29.4

---
+++ 2_Quellcode/(Um-)Rechnungstool/src/Front.java
@@ -0,0 +1,32 @@
+
+public class Front {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double l =4;
+		boolean cm = true;
+		boolean m = false;
+		boolean km = false;
+				
+		System.out.println(Length.length(l));
+		System.out.println(Length.length(l));
+		
+		
+		double g = 2;
+		System.out.println(l+" kg sind "+Weight.weight(g)+" pfund");
+
+		//double f = 2;
+		//System.out.println(f+" m^2 sind "+Flaeche.berechneflaeche(f)+" yard^2");
+
+		//double v = 2;
+		//System.out.println(v+" m^3 sind "+Volumen.berechnevolumen(v)+" yard^3");
+
+
+
+
+
+
+
+	}
+
+}
