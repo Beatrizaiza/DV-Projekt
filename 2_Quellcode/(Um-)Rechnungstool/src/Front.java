8e67288a485ccc51e75ece3c43d5932288a57704
lucawld <102234131+lucawld@users.noreply.github.com>
2022-05-10 16:05:34 +0200

    Versuche zu Dropdown Menüs

--- 2_Quellcode/(Um-)Rechnungstool/src/Front.java
+++ 2_Quellcode/(Um-)Rechnungstool/src/Front.java
@@ -1,3 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
 
 public class Front {
 
@@ -7,6 +13,41 @@ public class Front {
 		boolean cm = true;
 		boolean m = false;
 		boolean km = false;
+		
+		//Drop-Down Versuch 1
+		String[] convertingoptions = {"Wählen...", "Länge", "Fläche", "Volumen", "Gewicht"};
+
+		        String getconvertingsize = (String) JOptionPane.showInputDialog(
+		                null,
+		                "Bitte wähle die gewünschte Größe:",
+		                "Einheitenumrechner",
+		                JOptionPane.QUESTION_MESSAGE,
+		                null,
+		                convertingoptions,
+		                convertingoptions[0]);
+
+		        System.out.println("Gewählte Größe:" + getconvertingsize);
+		//Drop-Down Versuch 1
+		        
+		//Drop-Down Versuch 2
+		        
+//		        JComboBox<String> box = new JComboBox<String>();
+//				box.addItem("lba");
+//				box.addItem("blup");
+//				//box.removeAllItems(); // zum entfernen aller Auswahlmöglichkeiten
+//				pane.add(box);
+//				box.setBounds(0,0, 200, 100);
+//				box.addActionListener(new ActionListener() {
+//					
+//					@Override
+//					public void actionPerformed(ActionEvent e) {
+//						String s = (String) box.getSelectedItem();
+//						System.out.println("selected: "+s);
+//						
+//					}
+//				});
+		
+		//Drop-Down Versuch 2
 				
 		System.out.println(Length.length(l));
 		System.out.println(Length.length(l));
