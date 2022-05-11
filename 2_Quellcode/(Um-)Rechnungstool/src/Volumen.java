ee61f78178ddcb3895d32de942060074c1749690
JasminBin <jasmin.helene.binkowski@hs-furtwangen.de>
2022-05-11 21:33:30 +0200

    verbessert

--- 2_Quellcode/(Um-)Rechnungstool/src/Volumen.java
+++ 2_Quellcode/(Um-)Rechnungstool/src/Volumen.java
@@ -7,35 +7,38 @@ public class Volumen {
 		double l; //laenge
 		double h; //hoehe
 		
-
 		
-		public double pyramidVolume(double l, double h) //Pyramide
-		{
-			return ((1.0/3.0) * Math.pow(l, 2) * h);
-		}
-	
-		public double cubeVolume(double l) //Wuerfel
-		{
-			return l*l*l;
-		}
+		
+		
+		
 			
-		public double sphereVolume(double r) //Kugel
-		{
-			return ((4.0/3.0) * Math.PI * Math.pow(r,  3));
-		}
-		
-		public double cylinderVolume(double r, double h) //Zylinder
-		{
-			return (Math.PI * Math.pow(r,  2)*h);
-		}
-		
-		public double coneVolume(double r, double h) //Kegel
-		{
-			return (h * Math.pow(r, 2) * Math.PI/3);
-		}
+	}
+	public static double pyramidVolume(double l, double h) //Pyramide
+	{
+		return ((1.0/3.0) * Math.pow(l, 2) * h);
+	}
 
+	public static double cubeVolume(double l) //Wuerfel
+	{
+		return l*l*l;
+	}
+		
+	public static double sphereVolume(double r) //Kugel
+	{
+		return ((4.0/3.0) * Math.PI * Math.pow(r,  3));
+	}
 	
+	public static double cylinderVolume(double r, double h) //Zylinder
+	{
+		return (Math.PI * Math.pow(r,  2)*h);
 	}
+	
+	public static double coneVolume(double r, double h) //Kegel
+	{
+		return (h * Math.pow(r, 2) * Math.PI/3);
 	}
+
+
+}
 
 
