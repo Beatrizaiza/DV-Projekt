f33a3599a9c787b23bce754cdd20b2ab2710d533
HugSo <101948339+HugSo@users.noreply.github.com>
2022-05-12 11:35:07 +0200

    Update Flaechen.java
    Adds more area options

--- 2_Quellcode/(Um-)Rechnungstool/src/Flaechen.java
+++ 2_Quellcode/(Um-)Rechnungstool/src/Flaechen.java
@@ -11,7 +11,7 @@ public class Flaechen {
        
 	}
 	
-	// gonna do this for each the areas the user needs to compute
+	// Areas user might need to compute:
 	public static double rectArea(double w, double h)
 	{
 		return w*h;
@@ -22,14 +22,31 @@ public class Flaechen {
 		return Math.PI*r*r;
 	}
 	
+	public static double sectorCircArea(double r, double alpha) // Circular sector = Kreissektor =  Kreisausschnitt
+	{
+		double alphaRad = Math.toRadians(alpha);
+		return 0.5*Math.pow(r, 2)*alphaRad;
+	}
+	
 	public static double squareArea(double h) // yes I'm calling it h...it doesn't matter
 	{
 		return h*h;
 	}
 	
+	public static double parallelogArea(double a, double b, double alpha) // Parallelogram Area - put a diagram so user knows what is 
+	{																		// meant with the a, b and alpha 
+		double inRadians = Math.toRadians(alpha); // transform degree to radiant Java's uses radiant for sine calculation 
+		return a*b*Math.sin(inRadians);
+	}
+		
 	public static double triangleArea(double b, double h) 
 	{
 		return (b*h)/2;
+	}
+	
+	public static double equilatTriaArea(double b)  // Equilateral triangle = gleichseitiges Dreieck
+	{
+		return (Math.sqrt(3)/4)*Math.pow(b,2);
 	}
 	
 }
