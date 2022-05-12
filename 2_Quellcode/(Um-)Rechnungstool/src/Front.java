af2b5830ad59c48d0cb4d5f0ac036f9166f70af5
leonwelker <102161952+leonwelker@users.noreply.github.com>
2022-05-12 16:39:25 +0200

    start hash map

--- 2_Quellcode/(Um-)Rechnungstool/src/Front.java
+++ 2_Quellcode/(Um-)Rechnungstool/src/Front.java
@@ -8,12 +8,12 @@ public class Front {
 		boolean m = false;
 		boolean km = false;
 				
-		System.out.println(Length.length(l));
-		System.out.println(Length.length(l));
+		System.out.println(Berechnung.length(l));
 		
 		
-		double g = 2;
-		System.out.println(l+" kg sind "+Weight.weight(g)+" pfund");
+		
+		//double g = 2;
+		//System.out.println(l+" kg sind "+Weight.weight(g)+" pfund");
 
 		//double f = 2;
 		//System.out.println(f+" m^2 sind "+Flaeche.berechneflaeche(f)+" yard^2");
