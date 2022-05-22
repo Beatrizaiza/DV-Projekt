7b43ae06363d449fe28113f02ac305597c4a3003
leonwelker <102161952+leonwelker@users.noreply.github.com>
2022-05-22 13:07:51 +0200

    anpassungen einheitenrechner

--- 2_Quellcode/(Um-)Rechnungstool/src/Berechnung.java
+++ 2_Quellcode/(Um-)Rechnungstool/src/Berechnung.java
@@ -9,6 +9,7 @@ public class Berechnung extends Front {
 
 		Rechnung.put("mm", 0.001);
 		Rechnung.put("cm", 0.01);
+		Rechnung.put("dm", 0.1);
 		Rechnung.put("m", 1.0);
 		Rechnung.put("km", 1000.0);
 
@@ -19,6 +20,7 @@ public class Berechnung extends Front {
 
 		Rechnung.put("mm^2", 0.00001);
 		Rechnung.put("cm^2", 0.0001);
+		Rechnung.put("dm^2", 0.01);
 		Rechnung.put("m^2", 1.0);
 		Rechnung.put("km^2", 10000000.0);
 
@@ -30,6 +32,7 @@ public class Berechnung extends Front {
 		Rechnung.put("mg", 0.000001);
 		Rechnung.put("g", 0.001);
 		Rechnung.put("kg", 1.0);
+		Rechnung.put("Tonne", 1000.0);
 
 		Rechnung.put("Stone", 0.15748031496);
 		Rechnung.put("Pfund", 2.205);
@@ -37,8 +40,8 @@ public class Berechnung extends Front {
 
 // if bedingung für Länge 
 
-	if (Rechnung.get == "mm" || Rechnung.get == "cm" || Rechnung.get == "m" || Rechnung.get == "km") { 
-		double lu = (l * Rechnung.get("cm")) * Rechnung.get("yards"); // in die Klammern muss der vom User gewählte
+	if (Rechnung.get == "mm" || Rechnung.get == "cm" || Rechnung.get == "dm" ||Rechnung.get == "m" || Rechnung.get == "km") { 
+		double lu = (l * Rechnung.get("??")) * Rechnung.get("??"); // in die Klammern muss der vom User gewählte
 		System.out.println(l + " " + Rechnung.keySet() + "sind " + lu + " " + Rechnung.get("value4"));
 		} 
 	
